4ecbcf8e3f3a703bfd26729f14117436a50f9e4a
shashi8544 <shashi19092003ranjan@gmail.com>
2022-09-13 17:48:09 +0530

    assignment recived

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,5 +1,26 @@
-a=int(input("Enter Your Number:\n"))
-ans=1 
-for x in range (1,a+1):
-    ans=ans*x
-print(ans)
+<<<<<<< HEAD
+<<<<<<< HEAD
+print("hello world")
+=======
+def factorial(int x):
+    
+
+x=int(input("Enter the number whose factorial is to be found"))
+factorial(x)
+>>>>>>> a093f6a756f03b64e4d771512ac162920d2e4807
+=======
+def octact_identification(mod=5000):
+###Code
+
+
+from platform import python_version
+ver = python_version()
+
+if ver == "3.8.10":
+    print("Correct Version Installed")
+else:
+    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+
+mod=5000
+octact_identification(mod)
+>>>>>>> 63a3b8790ff61b4d005ae57e6e7704324f040548
