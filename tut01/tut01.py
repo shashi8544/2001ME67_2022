d76e83ce0730043de84c41173ec8b9c53add0887
shashi8544 <shashi19092003ranjan@gmail.com>
2022-09-15 01:18:33 +0530

    code after calculating overall count

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -30,3 +30,32 @@ try:
   Wavg=Wavg/count2
 except:
   print("there is typo in average calculation")
+df=[Uavg,Vavg,Wavg]
+ls=[]
+for i in range(0,29745):
+  ls.append([val1[i][0],val1[i][1]-Uavg,val1[i][2]-Vavg,val1[i][3]-Wavg])
+for i in range(0,29745):
+  if(ls[i][1]>=0):
+    if(ls[i][2]>=0):
+      if(ls[i][3]>=0):
+        ls[i].append(1)
+      else:
+        ls[i].append(-1)
+    else:
+      if(ls[i][3]>=0):
+        ls[i].append(4)
+      else:
+        ls[i].append(-4)
+  else:
+    if(ls[i][2]>=0):
+      if(ls[i][3]>=0):
+        ls[i].append(2)
+      else:
+        ls[i].append(-2)
+    else:
+      if(ls[i][3]>=0):
+        ls[i].append(3)
+      else:
+        ls[i].append(-3)
+lsdf=pd.DataFrame(ls,columns=['Time',"U'","V'","W'",'Octant'])
+dic={'1':0,'-1':0,'2':0,'-2':0,'3':0,'-3':0,'4':0,'-4':0}
