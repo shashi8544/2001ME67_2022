19d61f2f461518d86443d622ad0fc0ca0d327ce4
shashi8544 <shashi19092003ranjan@gmail.com>
2022-11-18 20:29:01 +0530

    defining a tutorial 2 function and importing its codes

--- tut07/tut07.py
+++ tut07/tut07.py
@@ -531,3 +531,329 @@ def tut4(wb,mod):
 
     except:
         print("there is error in uploading time stamp in excel")
+
+#### tutorial 2 definition
+def tut2(wb,mod):
+        #### calculating average value
+    try:
+        sheet = wb.active
+        Uavg=0
+        Vavg=0
+        Wavg=0
+        ls=[]
+        for row in sheet.iter_rows(min_row=1, min_col=1, max_row=19888, max_col=4):
+            lst1=[]
+            for cell in row:
+                lst1.append(cell.value)
+            ls.append(lst1)
+        for i in range(1,19888):
+            Uavg=Uavg+ls[i][1]
+            Vavg=Vavg+ls[i][2]
+            Wavg=Wavg+ls[i][3]
+        Uavg=Uavg/19887
+        Vavg=Vavg/19887
+        Wavg=Wavg/19887
+
+        lst_avg=[["Uavg","Vavg","Wavg"],[Uavg,Vavg,Wavg]]
+    except:
+        print("there is error in calculating average value")
+        exit()
+    #####creating octant of given value
+
+    try:
+        i=0
+        for row in sheet.iter_rows(min_row=1, min_col=5, max_row=2, max_col=7):
+            j=0
+            for cell in row:
+                cell.value=lst_avg[i][j]
+                j=j+1
+            i=i+1
+        lst_newval=[]
+        for i in range(1,19888):
+            lst_newval.append([ls[i][1]-Uavg,ls[i][2]-Vavg,ls[i][3]-Wavg])
+        lst_octant = []
+        for p in lst_newval:
+            if(p[0]>=0):
+                if(p[1]>=0):
+                    if(p[2]>=0):
+                        lst_octant.append(1)
+                    else:
+                        lst_octant.append(-1)
+                else:
+                    if(p[2]>=0):
+                        lst_octant.append(4)
+                    else:
+                        lst_octant.append(-4)
+            else:
+                if(p[1]>=0):
+                    if(p[2]>=0):
+                        lst_octant.append(2)
+                    else:
+                        lst_octant.append(-2)
+                else:
+                    if(p[2]>=0):
+                        lst_octant.append(3)
+                    else:
+                        lst_octant.append(-3)
+    except:
+        print("there is error in creating octant value")
+        exit()
+
+    ###### there is some error in creating overall count
+
+    try:
+        t=0
+        if(19888%mod==0):
+            t=19888//mod
+        else:
+            t=(19888//mod)+1
+
+        tt=str(mod)
+        lst_overall_count = [0,0,0,0,0,0,0,0]
+        for valu in lst_octant:
+            if valu==1:
+                lst_overall_count[0]=lst_overall_count[0]+1
+            if valu==-1:
+                lst_overall_count[1]=lst_overall_count[1]+1
+            if valu==2:
+                lst_overall_count[2]=lst_overall_count[2]+1
+            if valu==-2:
+                lst_overall_count[3]=lst_overall_count[3]+1
+            if valu==3:
+                lst_overall_count[4]=lst_overall_count[4]+1
+            if valu==-3:
+                lst_overall_count[5]=lst_overall_count[5]+1
+            if valu==4:
+                lst_overall_count[6]=lst_overall_count[6]+1
+            if valu==-4:
+                lst_overall_count[7]=lst_overall_count[7]+1
+        lst_hh=[]
+        for j in range(t):
+            if(j==t-1):
+                ttm1=str(mod*j)
+                ttm=ttm1+"-"+"19887"
+                lst_hh.append(ttm)
+            else:
+                if(j==0):
+                    ttm1=".0000"
+                else:
+                    ttm1=str(mod*j) 
+                ttm2=str(mod*(j+1)-1)
+                ttm=ttm1+"-"+ttm2
+                lst_hh.append(ttm) 
+        lst_hh.append("Verified")
+    except:
+        print("there is error in creating overall count value and verifing it")
+        exit()
+
+    ##### updating overall count and transition
+    try:
+        lst_hh_val=[]
+        for j in range(t):
+            lst_hh_temp=[0,0,0,0,0,0,0,0]
+            if(j==t-1):
+                y=19887
+            else:
+                y=mod*(j+1)
+            for valu in range(mod*j,y):
+                if lst_octant[valu]==1:
+                    lst_hh_temp[0]=lst_hh_temp[0]+1
+                if lst_octant[valu]==-1:
+                    lst_hh_temp[1]=lst_hh_temp[1]+1
+                if lst_octant[valu]==2:
+                    lst_hh_temp[2]=lst_hh_temp[2]+1
+                if lst_octant[valu]==-2:
+                    lst_hh_temp[3]=lst_hh_temp[3]+1
+                if lst_octant[valu]==3:
+                    lst_hh_temp[4]=lst_hh_temp[4]+1
+                if lst_octant[valu]==-3:
+                    lst_hh_temp[5]=lst_hh_temp[5]+1
+                if lst_octant[valu]==4:
+                    lst_hh_temp[6]=lst_hh_temp[6]+1
+                if lst_octant[valu]==-4:
+                    lst_hh_temp[7]=lst_hh_temp[7]+1
+
+
+            lst_hh_val.append(lst_hh_temp)
+
+        lst_verified=[0,0,0,0,0,0,0,0]
+        for i in range(t):
+            for j in range(8):
+                lst_verified[j]=lst_verified[j]+lst_hh_val[i][j]
+
+    except:
+        print("there is error in updating in verified count")
+
+
+    #### creating list for mod transition value
+    try:
+        lst_tran_count=[]
+
+        for i in range(t):
+            if(i==t-1):
+                y=19886
+            else:
+                y=mod*(i+1)-1
+            lst_tran_count_temp=[]
+            temp_1={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_11={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_2={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_22={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_3={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_33={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_4={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_44={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            for k in range(mod*i,y):
+
+                if(lst_octant[k]==1):
+                    temp_1[lst_octant[k+1]]=temp_1[lst_octant[k+1]]+1
+                if(lst_octant[k]==-1):
+                    temp_11[lst_octant[k+1]]=temp_11[lst_octant[k+1]]+1
+                if(lst_octant[k]==2):
+                    temp_2[lst_octant[k+1]]=temp_2[lst_octant[k+1]]+1
+                if(lst_octant[k]==-2):
+                    temp_22[lst_octant[k+1]]=temp_22[lst_octant[k+1]]+1
+                if(lst_octant[k]==3):
+                    temp_3[lst_octant[k+1]]=temp_3[lst_octant[k+1]]+1
+                if(lst_octant[k]==-3):
+                    temp_33[lst_octant[k+1]]=temp_33[lst_octant[k+1]]+1
+                if(lst_octant[k]==4):
+                    temp_4[lst_octant[k+1]]=temp_4[lst_octant[k+1]]+1
+                if(lst_octant[k]==-4):
+                    temp_44[lst_octant[k+1]]=temp_44[lst_octant[k+1]]+1
+            lst_tran_count_temp.append(temp_1)
+            lst_tran_count_temp.append(temp_11)
+            lst_tran_count_temp.append(temp_2)
+            lst_tran_count_temp.append(temp_22)
+            lst_tran_count_temp.append(temp_3)
+            lst_tran_count_temp.append(temp_33)
+            lst_tran_count_temp.append(temp_4)
+            lst_tran_count_temp.append(temp_44)
+            lst_tran_count.append(lst_tran_count_temp)
+            lst_tran_count_temp=[]
+            temp_1={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_11={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_2={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_22={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_3={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_33={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_4={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_44={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+
+        lst_tran_count1=[]
+
+        for i in range(t):
+
+            lst_tran_count_temp=[]
+            temp_1={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_11={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_2={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_22={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_3={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_33={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_4={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_44={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            for k in range(19886):
+
+                if(lst_octant[k]==1):
+                    temp_1[lst_octant[k+1]]=temp_1[lst_octant[k+1]]+1
+                if(lst_octant[k]==-1):
+                    temp_11[lst_octant[k+1]]=temp_11[lst_octant[k+1]]+1
+                if(lst_octant[k]==2):
+                    temp_2[lst_octant[k+1]]=temp_2[lst_octant[k+1]]+1
+                if(lst_octant[k]==-2):
+                    temp_22[lst_octant[k+1]]=temp_22[lst_octant[k+1]]+1
+                if(lst_octant[k]==3):
+                    temp_3[lst_octant[k+1]]=temp_3[lst_octant[k+1]]+1
+                if(lst_octant[k]==-3):
+                    temp_33[lst_octant[k+1]]=temp_33[lst_octant[k+1]]+1
+                if(lst_octant[k]==4):
+                    temp_4[lst_octant[k+1]]=temp_4[lst_octant[k+1]]+1
+                if(lst_octant[k]==-4):
+                    temp_44[lst_octant[k+1]]=temp_44[lst_octant[k+1]]+1
+            lst_tran_count_temp.append(temp_1)
+            lst_tran_count_temp.append(temp_11)
+            lst_tran_count_temp.append(temp_2)
+            lst_tran_count_temp.append(temp_22)
+            lst_tran_count_temp.append(temp_3)
+            lst_tran_count_temp.append(temp_33)
+            lst_tran_count_temp.append(temp_4)
+            lst_tran_count_temp.append(temp_44)
+            lst_tran_count1.append(lst_tran_count_temp)
+            lst_tran_count_temp=[]
+            temp_1={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_11={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_2={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_22={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_3={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_33={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_4={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+            temp_44={1:0,-1:0,2:0,-2:0,3:0,-3:0,4:0,-4:0}
+    except:
+        print("there is some error in creating list of mod transition value")
+
+
+    #### updating mod transition value
+    try:
+        lst_skelton_1=[[nan,"Overall Transition Count",nan,nan,nan,nan,nan,nan,nan,nan],[nan,nan,"To",nan,nan,nan,nan,nan,nan,nan],[nan,"count","+1","-1","+2","-2","+3","-3","+4","-4"],["From","+1",nan,nan,nan,nan,nan,nan,nan,nan],[nan,"-1",nan,nan,nan,nan,nan,nan,nan,nan],[nan,"+2",nan,nan,nan,nan,nan,nan,nan,nan],[nan,"-2",nan,nan,nan,nan,nan,nan,nan,nan],[nan,"+3",nan,nan,nan,nan,nan,nan,nan,nan],[nan,"-3",nan,nan,nan,nan,nan,nan,nan,nan],[nan,"+4",nan,nan,nan,nan,nan,nan,nan,nan],[nan,"-4",nan,nan,nan,nan,nan,nan,nan,nan]]
+        lst_skelton=[[nan,"Mod Transition count",nan,nan,nan,nan,nan,nan,nan,nan],[nan,nan,"To",nan,nan,nan,nan,nan,nan,nan],[nan,"count","+1","-1","+2","-2","+3","-3","+4","-4"],["From","+1",nan,nan,nan,nan,nan,nan,nan,nan],[nan,"-1",nan,nan,nan,nan,nan,nan,nan,nan],[nan,"+2",nan,nan,nan,nan,nan,nan,nan,nan],[nan,"-2",nan,nan,nan,nan,nan,nan,nan,nan],[nan,"+3",nan,nan,nan,nan,nan,nan,nan,nan],[nan,"-3",nan,nan,nan,nan,nan,nan,nan,nan],[nan,"+4",nan,nan,nan,nan,nan,nan,nan,nan],[nan,"-4",nan,nan,nan,nan,nan,nan,nan,nan]]
+        i=0
+        zz=0
+        for row in sheet.iter_rows(min_row=zz, min_col=34, max_row=zz+10, max_col=43):
+            j=0
+            for cell in row:
+                cell.value=lst_skelton_1[i][j]
+                j=j+1
+            i=i+1
+        for h in range(1,t+1):
+            i=0
+            for row in sheet.iter_rows(min_row=13*h+zz, min_col=34, max_row=13*(h)+zz+10, max_col=43):
+                j=0
+                for cell in row:
+                    cell.value=lst_skelton[i][j]
+                    j=j+1
+                i=i+1
+
+        lst_tran_count_ans=[]
+        for lsss in lst_tran_count:
+            list_temp_1=[]
+            for lsss1 in lsss:
+                list_temp=[]
+                for x in lsss1:
+                    list_temp.append(lsss1[x])
+                list_temp_1.append(list_temp)
+            lst_tran_count_ans.append(list_temp_1)
+        lst_tran_count_ans1=[]
+        for lsss in lst_tran_count1:
+            list_temp_1=[]
+            for lsss1 in lsss:
+                list_temp=[]
+                for x in lsss1:
+                    list_temp.append(lsss1[x])
+                list_temp_1.append(list_temp)
+            lst_tran_count_ans1.append(list_temp_1)
+
+        for h in range(1,t+1):
+            i=0
+            for row in sheet.iter_rows(min_row=13*(h)+zz+3, min_col=36, max_row=13*(h)+zz+10, max_col=43):
+                j=0
+                for cell in row:
+                    cell.value=lst_tran_count_ans[h-1][i][j]
+                    j=j+1
+                i=i+1
+        i=0
+        for h in range(1,t+1):
+            for row in sheet.iter_rows(min_row=13*(h)+zz+1, min_col=35, max_row=13*(h)+zz+1, max_col=35):
+                for cell in row:
+                    cell.value=lst_hh[i]    
+                i=i+1
+
+        i=0
+        for row in sheet.iter_rows(min_row=zz+3, min_col=36, max_row=zz+10, max_col=43):
+            j=0
+            for cell in row:
+                cell.value=lst_tran_count_ans1[0][i][j]
+                j=j+1
+            i=i+1
+    except:
+        print("there is error in updating mod transition value in excel")
