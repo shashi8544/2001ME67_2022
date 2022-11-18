a2f6c2a87a8be8f7babe2af297d0534d0e75ed05
shashi8544 <shashi19092003ranjan@gmail.com>
2022-11-18 20:28:07 +0530

    defining a tut4 function and importing codes from tut4

--- tut07/tut07.py
+++ tut07/tut07.py
@@ -315,3 +315,219 @@ def tut5(wb,mod):
     except:
         print("there is some errror in creating skelton and list of rank")
 
+
+#### tutorial 4 definition
+def tut4(wb,mod):
+        #### calculating average value
+    sheet = wb.active
+    try:
+        Uavg=0
+        Vavg=0
+        Wavg=0
+        ls=[]
+        for row in sheet.iter_rows(min_row=1, min_col=1, max_row=19888, max_col=4):
+            lst1=[]
+            for cell in row:
+                lst1.append(cell.value)
+            ls.append(lst1)
+        for i in range(1,19888):
+            Uavg=Uavg+ls[i][1]
+            Vavg=Vavg+ls[i][2]
+            Wavg=Wavg+ls[i][3]
+        Uavg=Uavg/19887
+        Vavg=Vavg/19887
+        Wavg=Wavg/19887
+
+        lst_avg=[["Uavg","Vavg","Wavg"],[Uavg,Vavg,Wavg]]
+    except:
+        print("there is error in calculating average value")
+        exit()
+
+    #####creating octant of given value
+
+    try:
+        i=0
+        for row in sheet.iter_rows(min_row=1, min_col=5, max_row=2, max_col=7):
+            j=0
+            for cell in row:
+                cell.value=lst_avg[i][j]
+                j=j+1
+            i=i+1
+        lst_newval=[]
+        for i in range(1,19888):
+            lst_newval.append([ls[i][1]-Uavg,ls[i][2]-Vavg,ls[i][3]-Wavg])   
+
+        lst_octant = []
+        for p in lst_newval:
+            if(p[0]>=0):
+                if(p[1]>=0):
+                    if(p[2]>=0):
+                        lst_octant.append(1)
+                    else:
+                        lst_octant.append(-1)
+                else:
+                    if(p[2]>=0):
+                        lst_octant.append(4)
+                    else:
+                        lst_octant.append(-4)
+            else:
+                if(p[1]>=0):
+                    if(p[2]>=0):
+                        lst_octant.append(2)
+                    else:
+                        lst_octant.append(-2)
+                else:
+                    if(p[2]>=0):
+                        lst_octant.append(3)
+                    else:
+                        lst_octant.append(-3)
+
+    except:
+        print("there is error in creating octant value")
+        exit()
+
+
+
+    ### creating skelton table
+    try:
+        row_head = ["count","Longest Subsquence Length","count"]
+        row_column = ["+1","-1","+2","-2","+3","-3","+4","-4"]
+        for row in sheet.iter_rows(min_row=2, min_col=45, max_row=2, max_col=47):
+            j=0
+            for cell in row:
+                cell.value=row_head[j]
+                j=j+1
+        for row in sheet.iter_rows(min_row=2, min_col=49, max_row=2, max_col=51):
+            j=0
+            for cell in row:
+                cell.value=row_head[j]
+                j=j+1
+
+        i=0
+        for row in sheet.iter_rows(min_row=3, min_col=45, max_row=10, max_col=45):
+            for cell in row:
+                cell.value=row_column[i]
+            i=i+1
+    except:
+        print("there is error in creating skelton of table")
+
+
+
+
+    #####  subsequence codes 
+    try:
+        prev1=[1,-1,2,-2,3,-3,4,-4]
+        ans=[]
+
+        for prev in prev1:
+            count1_max=0
+            count1=0
+            temp_count=0
+            for v in lst_octant:
+                if(v==prev):
+                    temp_count=temp_count+1
+                else:
+                    if(temp_count>count1_max):
+                        count1=1
+                        count1_max=temp_count
+                    elif(count1_max==temp_count & count1_max!=0):
+                        count1=count1+1
+                    temp_count=0
+            lst_temp=[count1_max,count1]
+            ans.append(lst_temp)
+            count1_max=0
+            count1=0
+            temp_count=0
+    except:
+        print("there is some error in codes of subsequence")
+
+
+    ##### updating subsequence count in excel
+    try:
+        i=0
+        for row in sheet.iter_rows(min_row=3, min_col=46, max_row=10, max_col=47):
+            j=0
+            for cell in row:
+                cell.value=ans[i][j]
+                j=j+1
+            i=i+1
+    except:
+        print("there is some error in updating excel file")
+
+    #### finding timestamp of maximum subsequence
+    res=[]
+    count_line=0
+    list_temp=["Time","From","To"]
+    for i in range(8):
+        list_temp_1=[]
+        list_temp_1.append(row_column[i])
+        list_temp_1.append(ans[i][0])
+        list_temp_1.append(ans[i][1])
+        res.append(list_temp_1)
+
+
+    #########creating list of time stamp
+    try:   
+        new_time_list=[]
+        x=0.0
+        y=0.0
+        for i in range(1,len(ls)):
+            new_time_list.append(ls[i][0])
+        time_list=[]
+        temp=[1,-1,2,-2,3,-3,4,-4]
+        for i in range(8):
+            time_list_1=[]
+            temp_count=0
+            for j in range(19887):
+                if(temp_count==ans[i][0]):
+                    z=[x,y]
+                    time_list_1.append(z)
+                    x=0.0
+                    y=0.0
+                    temp_count=0
+                if(temp[i]==lst_octant[j]):
+                    if(temp_count==0):
+                        x=new_time_list[j]
+                        y=new_time_list[j]
+                    else:
+                        y=new_time_list[j]
+                    temp_count=temp_count+1
+                else:
+                    x=0.0
+                    y=0.0
+                    temp_count=0
+            time_list.append(time_list_1)
+    except:
+        print("Error in calculating time stamp")
+
+
+    ########  updating time stamp in excel
+    try:
+        ro=3
+        for i in range(8):
+            for row in sheet.iter_rows(min_row=ro, min_col=49, max_row=ro, max_col=51):
+                j=0
+                for cell in row:
+                    cell.value=res[i][j]
+                    cell.border=Border(left=Side(border_style='thin',color="000000"),right=Side(border_style='thin',color="000000"),top=Side(border_style='thin',color="000000"),bottom=Side(border_style='thin',color="000000"))
+                    j=j+1
+            ro=ro+1
+            for row in sheet.iter_rows(min_row=ro, min_col=49, max_row=ro, max_col=51):
+                j=0
+                for cell in row:
+                    cell.value=list_temp[j]
+                    cell.border=Border(left=Side(border_style='thin',color="000000"),right=Side(border_style='thin',color="000000"),top=Side(border_style='thin',color="000000"),bottom=Side(border_style='thin',color="000000"))
+                    j=j+1
+            ro=ro+1
+            for items in time_list[i]:
+                for row in sheet.iter_rows(min_row=ro, min_col=50, max_row=ro, max_col=51):
+                    j=0
+                    for cell in row:
+                        cell.value=items[j]
+                        cell.border=Border(left=Side(border_style='thin',color="000000"),right=Side(border_style='thin',color="000000"),top=Side(border_style='thin',color="000000"),bottom=Side(border_style='thin',color="000000"))
+                        cell.border=Border(left=Side(border_style='thin',color="000000"),right=Side(border_style='thin',color="000000"),top=Side(border_style='thin',color="000000"),bottom=Side(border_style='thin',color="000000"))
+                        j=j+1
+                ro=ro+1
+
+    except:
+        print("there is error in uploading time stamp in excel")
